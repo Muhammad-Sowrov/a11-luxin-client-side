44a4020a014999dfd6e2831f0afb89182b1d8474
Muhammad-Sowrov <muhammadsowrov53@gmail.com>
2023-11-09 15:27:24 +0600

    add frequenntly ased question

--- src/Pages/Faq/Faq.jsx
+++ src/Pages/Faq/Faq.jsx
@@ -8,43 +8,81 @@ const Faq = () => {
             1. What is the check-in and check-out time for the hotel room?
           </div>
           <div className="collapse-content">
-            <p>Check-in time is typically at 3:00 PM, and check-out is at 12:00 PM.</p>
+            <p>
+              Check-in time is typically at 3:00 PM, and check-out is at 12:00
+              PM.
+            </p>
           </div>
         </div>
         <div className="collapse collapse-arrow join-item border border-base-300">
           <input type="radio" name="my-accordion-4" />
           <div className="collapse-title text-xl font-medium">
-          2. Can I request an early check-in or late check-out? Is there an additional fee for this service?
+            2. Can I request an early check-in or late check-out? Is there an
+            additional fee for this service?
           </div>
           <div className="collapse-content">
-            <p>Early check-in and late check-out requests can often be accommodated but are subject to availability and may come with additional fees. It's best to contact the hotel in advance to arrange this.</p>
+            <p>
+              Early check-in and late check-out requests can often be
+              accommodated but are subject to availability and may come with
+              additional fees. It's best to contact the hotel in advance to
+              arrange this.
+            </p>
           </div>
         </div>
         <div className="collapse collapse-arrow join-item border border-base-300">
           <input type="radio" name="my-accordion-4" />
           <div className="collapse-title text-xl font-medium">
-          3. Do the hotel rooms have Wi-Fi, and is it complimentary?
+            3. Do the hotel rooms have Wi-Fi, and is it complimentary?
           </div>
           <div className="collapse-content">
-            <p>Most hotels offer complimentary Wi-Fi in their rooms. You can find the Wi-Fi network and password in the room or by asking at the front desk.</p>
+            <p>
+              Most hotels offer complimentary Wi-Fi in their rooms. You can find
+              the Wi-Fi network and password in the room or by asking at the
+              front desk.
+            </p>
           </div>
         </div>
         <div className="collapse collapse-arrow join-item border border-base-300">
           <input type="radio" name="my-accordion-4" />
           <div className="collapse-title text-xl font-medium">
-          Is room service available, and what are the hours of operation?
+            Is room service available, and what are the hours of operation?
           </div>
           <div className="collapse-content">
-            <p>Room service is often available, and hours of operation are usually provided in your room. You can order food and beverages to your room for an added convenience.</p>
+            <p>
+              Room service is often available, and hours of operation are
+              usually provided in your room. You can order food and beverages to
+              your room for an added convenience.
+            </p>
+          </div>
+        </div>
+
+        <div className="collapse collapse-arrow join-item border border-base-300">
+          <input type="radio" name="my-accordion-4" />
+          <div className="collapse-title text-xl font-medium">
+            4. Is breakfast included in the room rate, and if not, what are the
+            dining options available on the property?
+          </div>
+          <div className="collapse-content">
+            <p>
+              Breakfast inclusion varies by hotel. If not included, there are
+              usually on-site restaurants or room service options for dining.
+              Prices and menus are available in the room or at the front desk.
+            </p>
           </div>
         </div>
         <div className="collapse collapse-arrow join-item border border-base-300">
           <input type="radio" name="my-accordion-4" />
           <div className="collapse-title text-xl font-medium">
-           4. What is the cancellation policy for a hotel room reservation, and are there any penalties for late cancellations or no-shows?
+            5. What is the cancellation policy for a hotel room reservation, and
+            are there any penalties for late cancellations or no-shows?
           </div>
           <div className="collapse-content">
-            <p>Cancellation policies can vary by hotel and rate type. It's important to review the specific terms of your reservation to understand the cancellation policy and any associated penalties for late cancellations or no-shows.</p>
+            <p>
+              Cancellation policies can vary by hotel and rate type. It's
+              important to review the specific terms of your reservation to
+              understand the cancellation policy and any associated penalties
+              for late cancellations or no-shows.
+            </p>
           </div>
         </div>
       </div>
